1954b15e771974d4ce6efe8c02afc31344a7a32d
Tianhao Zhou <tianhaoz@umich.edu>
2018-04-10 21:26:30 -0700

    fix bug

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -122,7 +122,7 @@ RUN curl --silent --show-error --location --fail --retry 3 --output /tmp/chromed
   && chmod +x /usr/local/bin/chromedriver"
 fi
 
-echo "Install ROS"
+echo "Install ROS core"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
